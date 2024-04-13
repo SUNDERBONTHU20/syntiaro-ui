f3b21dfadecd012c9251a468d1cd7c79f44e61ad
SUNDERBONTHU20 <sunderbonthu7@gmail.com>
2024-04-13 13:09:56 +0530

    done

---
+++ index.js
@@ -0,0 +1,3 @@
+export { default } from './src/index';
+// export {default} from './src/style.module.css'
+// export * as default from './src/Button'
---
+++ src/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { cva } from "class-variance-authority";
+import style from './style.module.css';
+const button = cva(css.base, {
+    variants: {
+        variant: {
+            primary: style.primary,
+            secondary: style.secondary,
+            success: style.success,
+            danger: style.danger
+        }, //intent->default classes primary,secondary
+        size: {
+            small: style.small,
+            medium: style.medium,
+            large: style.large
+        }
+    },
+    defaultVariants: {
+        variant: "primary",
+        size: "medium"
+    }
+})
+// eslint-disable-next-line react/prop-types
+const Button = ({ value, variant, className, as, href, size, ...props }) => {
+    if (size) {
+        if (size === "sm") {
+            size = "small";
+        }
+        if (size === "lg") {
+            size = "large"
+        }
+        if (size === "medium") {
+            size = "medium"
+        }
+    }
+    if (as === "input") {
+        return <input className={button({ variant, size, className })} value={value} {...props} />
+    }
+    if (href) {
+        return <a href={href} role="button" className={button({ variant, size, className })} {...props}>{value}</a>
+    }
+    return (
+        <>
+            <button className={button({ variant, size, className })}  {...props}>{value}</button>
+        </>
+    )
+};
+
+export default Button;
